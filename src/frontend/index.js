db223e167efaeb451b091ec7e95fa0a7730932b5
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2025-01-25 01:50:32 +0000

    dsa

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,15 +1,5 @@
-import { backend } from "declarations/backend";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-  // Interact with backend actor, calling the greet method
-  const greeting = await backend.greet(name);
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-  return false;
-});
+ReactDOM.render(<App />, document.getElementById("root"));
