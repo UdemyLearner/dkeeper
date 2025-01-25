1547df0a59559c7a369f9a0373be1e064387c815
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2025-01-25 14:00:04 +0000

    Completed

--- dkeeper/src/dkeeper_backend/main.mo
+++ dkeeper/src/dkeeper_backend/main.mo
@@ -1,13 +1,14 @@
 import Text "mo:base/Text";
 import List "mo:base/List";
 import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
 actor DKeeper{
   public type Note = {
     title: Text;
     content: Text;
   };
 
-  var notes: List.List<Note> = List.nil<Note>();
+  stable var notes: List.List<Note> = List.nil<Note>();
 
   public func createNote(titleText:Text, contentText: Text){
     let newNote: Note = {
@@ -16,6 +17,17 @@ actor DKeeper{
     };
     notes := List.push(newNote, notes);
     Debug.print(debug_show (notes));
+  };
+
+  public query func readNotes() : async [Note]{
+    return List.toArray(notes);
+  };
+
+  public func removeNotes(id :Nat){
+    let listFront = List.take(notes,id);
+    let listBack = List.drop(notes, id+1);
+    notes := List.append(listFront,listBack);
+    Debug.print(debug_show(notes));
   };
 
 }
--- dkeeper/src/dkeeper_frontend/src/App.jsx
+++ dkeeper/src/dkeeper_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -12,11 +12,22 @@ function App() {
   function addNote(newNote) {
     setNotes(prevNotes => {
       dkeeper_backend.createNote(newNote.title, newNote.content)
-      return [...prevNotes, newNote];
+      return [newNote,...prevNotes];
     });
   }
 
+  useEffect(() =>{
+    console.log("render effect triggered");
+    fetchData();
+  },[])
+
+  async function fetchData() {
+    const notesArray = await dkeeper_backend.readNotes();
+    setNotes(notesArray);
+  }
+
   function deleteNote(id) {
+    dkeeper_backend.removeNotes(id);
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
