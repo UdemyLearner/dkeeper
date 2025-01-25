3d883a09fee537bd53931db73e4330bdbfbcdd5f
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2025-01-25 02:46:03 +0000

    modified:   dkeeper/src/dkeeper_backend/main.mo 	modified:   dkeeper/src/dkeeper_frontend/src/App.jsx 	new file:   react_test

--- dkeeper/src/dkeeper_frontend/src/App.jsx
+++ dkeeper/src/dkeeper_frontend/src/App.jsx
@@ -3,12 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+// import {DKeeper} from "../../declarations/dkeeper_backend"
+import { dkeeper_backend } from "../../declarations/dkeeper_backend";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
     setNotes(prevNotes => {
+      dkeeper_backend.createNote(newNote.title, newNote.content)
       return [...prevNotes, newNote];
     });
   }
